2372046b80dfbbaf6213037edaddd5657be03912
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2021-02-20 18:37:52 -0500

    getting routed

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+ const  notes = require("../../db/db.json")
+ const { v4: uuidv4 } = require('uuid');
+
+router.get('/api/notes', (req, res) => {
+  let saved = notes;
+  console.log(saved)
+  res.json(saved);
+})
+
+router.post('/api/notes', (req, res) => {
+  req.body.id = uuidv4()
+  console.log(req.body)
+  notes.push(req.body)
+  res.json(notes);
+})
+
+router.delete('/api/notes/:id', (req, res) => {
+  deleteNote(notes, req.params.id);
+  res.json(notes);
+})
+
+
+module.exports = router;
